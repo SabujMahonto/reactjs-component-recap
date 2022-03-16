2bf118885af056763536f12cfcf185bce6024e4f
SabujMahonto <64306095+SabujMahonto@users.noreply.github.com>
2022-03-16 16:07:13 +0600

    component recap done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
 
+const producStyle = {
+ backgroundColor:'orange',
+ margin:'12px',
+ padding:'15px',
+ borderRadius:'20px',
+ border:'2px solid yellow',
+ textAlign:'center'
+
+}
+
+
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+       products.map(p => <Product name= {p.name} price={p.price} color={p.color} ></Product>)
+       
+       }
+
+
+      {/* <Product name="laptop" price="3333" color="silver" ></Product>
+      <Product name="laptop" price="3333" color="silver" ></Product>
+      <Product name="laptop" price="3333" color="silver" ></Product>
+      <Product name="laptop" price="3333" color="silver" ></Product>
+      <Product name="laptop" price="3333" color="silver" ></Product>
+      <Product name="laptop" price="3333" color="silver" ></Product> */}
     </div>
   );
+}
+
+
+const products = [
+  {name:'laptop', price:23900, color:'silver'},
+  {name:'laptop', price:23900, color:'silver'},
+  {name:'laptop', price:23900, color:'silver'},
+  {name:'laptop', price:23900, color:'silver'},
+  {name:'laptop', price:23900, color:'silver'},
+]
+
+function Product(props){
+  return(
+    <div style={producStyle}>
+      <h3>Name:{props.name}</h3>
+      <h4>price:{props.price}</h4>
+      <h6>color:{props.color}</h6>
+    </div>
+  )
 }
 
 export default App;
